6e959147a8f18b7044d9afc4fa9e049113464101
Martin Brandl <mjibrandl@gmail.com>
2020-01-07 13:33:20 +0100

    Added backup and restore for vscode keybindings

--- Backup-VsCodeUserSettings.ps1
+++ Backup-VsCodeUserSettings.ps1
@@ -4,4 +4,9 @@ New-Item $destination -ItemType Directory -Force | Out-Null
 Copy-Item `
     -Path (Join-Path $env:APPDATA 'Code\User\settings.json') `
     -Destination $destination `
+    -force
+
+Copy-Item `
+    -Path (Join-Path $env:APPDATA 'Code\User\keybindings.json') `
+    -Destination $destination `
     -force    
--- Restore-VsCodeUserSettings.ps1
+++ Restore-VsCodeUserSettings.ps1
@@ -2,3 +2,8 @@ Copy-Item `
     -Path (Join-Path $PSScriptRoot "settings\$($env:COMPUTERNAME)\settings.json").ToLower() `
     -Destination (Join-Path $env:APPDATA 'Code\User\settings.json') `
     -force
+
+Copy-Item `
+    -Path (Join-Path $PSScriptRoot "settings\$($env:COMPUTERNAME)\keybindings.json").ToLower() `
+    -Destination (Join-Path $env:APPDATA 'Code\User\keybindings.json') `
+    -force
